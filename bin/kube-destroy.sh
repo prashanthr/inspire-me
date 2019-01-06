991568f039fc05b674d35f4d6f73944b40a5ede3
Prash <prashanth@slice.com>
2019-01-06 12:57:41 -0800

    docker,kube fixes

--- bin/kube-destroy.sh
+++ bin/kube-destroy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-kubectl delete service $SERVICE_NAME
-# kubectl delete pods --all --namespace=$POD_NAME
+kubectl delete service $SERVICE_NAME --namespace=$NAMESPACE
+kubectl delete pods --all --namespace=$NAMESPACE
+kubectl delete deployments --all --namespace=$NAMESPACE
--- bin/kube-launch.sh
+++ bin/kube-launch.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 eval $(minikube docker-env)
 docker build -t $IMAGE_TAG .
-kubectl run $POD_NAME --image=$IMAGE_TAG --port=$PORT
-kubectl expose deployment $POD_NAME --type=NodePort --name=$SERVICE_NAME
-kubectl get deployments
+kubectl run $POD_NAME --image=$IMAGE_TAG --port=$PORT --namespace=$NAMESPACE
+kubectl expose deployment $POD_NAME --type=NodePort --name=$SERVICE_NAME --namespace=$NAMESPACE
 eval $(minikube docker-env -u)
+kubectl get deployments
 minikube service $SERVICE_NAME
