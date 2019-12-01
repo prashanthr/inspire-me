b0a0c88ccc421d7d4d4bf43a6e16f4e60860f967
Prashanth R <prashanthrdev@gmail.com>
2019-12-01 15:42:54 -0800

    remove unused

--- bin/infra/kube-destroy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-kubectl delete service $SERVICE_NAME --namespace=$NAMESPACE
-kubectl delete pods --all --namespace=$NAMESPACE
-kubectl delete deployments --all --namespace=$NAMESPACE
--- bin/infra/kube-launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-eval $(minikube docker-env)
-docker build -t $IMAGE_TAG .
-kubectl run $POD_NAME --image=$IMAGE_TAG --port=$PORT
-kubectl expose deployment $POD_NAME --type=NodePort --name=$SERVICE_NAME
-eval $(minikube docker-env -u)
-kubectl get deployments
-minikube service $SERVICE_NAME --vm-driver=hyperkit
