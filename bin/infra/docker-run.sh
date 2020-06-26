d3140ee9d5cdab2229c32d8d36230d1f98beb7f9
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-06-25 18:47:00 -0700

    nginx version

--- bin/infra/docker-run.sh
+++ bin/infra/docker-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ -z $DOCKER_PORT ]; then
-  DOCKER_PORT=9000
+  DOCKER_PORT=9005
 fi
 
 echo "Running docker container with tag $TAG and name $CONTAINER_NAME at port $DOCKER_PORT..."
-docker run  --name $CONTAINER_NAME -p $DOCKER_PORT:$DOCKER_PORT -p 80:$DOCKER_PORT -p 443:$DOCKER_PORT --rm $TAG
+docker run  --name $CONTAINER_NAME -p $DOCKER_PORT:$DOCKER_PORT --rm $TAG
--- deploy/nginx/default.lua
+++ deploy/nginx/default.lua
@@ -1,5 +1,20 @@
 server {
-    listen 9000;
+  listen 9005 default_server;
 
-    server_name localhost;
+  server_name comical.site;
+  root /var/www/deploy/app/build;
+
+  location / {
+      proxy_pass http://localhost:9000;
+      #proxy_http_version 1.1;
+      #proxy_set_header Upgrade $http_upgrade;
+      #proxy_set_header Connection 'upgrade';
+      #proxy_set_header Host $host;
+      #proxy_cache_bypass $http_upgrade;
+  }
+
+  location ^~ /.well-known {
+    allow all;
+    root  /var/www/deploy/app/;
+  }
 }
