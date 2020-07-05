ef4da15c2704f0d1e68fb3cf7b17b66efc17ac10
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-07-05 10:23:36 -0700

    Chore/k8 cleanup (#33)
    Co-authored-by: Prashanth R <1409501+prashanthr@users.noreply.github.com>

--- deploy/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# download cluster config file
-kubectl --kubeconfig=$KUBE_CONF_PATH get nodes
-kubectl --kubeconfig=$KUBE_CONF_PATH apply -f ./deploy/kube-config/production
-kubectl --kubeconfig=$KUBE_CONF_PATH get service inspire-me
-kubectl --kubeconfig=$KUBE_CONF_PATH get service inspire-me
