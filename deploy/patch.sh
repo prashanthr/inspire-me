ce7b29ee746ad403df56ad229fb4a3c7d70a1745
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-07-05 10:14:27 -0700

    chore(k8): Cleanup

--- deploy/patch.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-kubectl --namespace $KUBE_NAMESPACE patch deployment comical-site-app -p \
-  "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
