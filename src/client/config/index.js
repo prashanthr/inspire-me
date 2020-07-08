9945de455071ae690e4754b3dbb1e5f8eb290be8
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-07-08 12:09:33 -0700

    feat(footer): Add buy me a coffee link to footer (#34)
    Co-authored-by: Prashanth R <1409501+prashanthr@users.noreply.github.com>

--- src/client/config/index.js
+++ src/client/config/index.js
@@ -1,13 +1,19 @@
 const isProd = () => process.env.NODE_ENV.toLowerCase() === 'production'
 const localPort = process.env.REACT_APP_SERVICE_PORT || 9000
 
+const defaultConfig = {
+  coffeeUrl: 'https://www.buymeacoffee.com/TGuwXOA'
+}
+
 const localConfig = {
   apiBaseUrl: `http://localhost:${localPort}`,
-  servicePort: localPort
+  servicePort: localPort,
+  ...defaultConfig
 }
 
 const prodConfig = {
-  apiBaseUrl: 'https://comical.site'
+  apiBaseUrl: 'https://comical.site',
+  ...defaultConfig
 }
 
 const overrides = isProd() ? prodConfig : localConfig
